24220a50181196da555549e5354581f27af09aba
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-03-20 08:27:24 +0100

    small corrections on components

--- Angular-Course/01_Components_Part_I/end/app/components/navigation/navigation.component.js
+++ Angular-Course/01_Components_Part_I/end/app/components/navigation/navigation.component.js
@@ -8,20 +8,19 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = require("@angular/core");
+var core_1 = require('@angular/core');
 var NavigationComponent = (function () {
     function NavigationComponent() {
     }
+    NavigationComponent = __decorate([
+        core_1.Component({
+            moduleId: module.id,
+            selector: 'navigation',
+            templateUrl: './navigation.component.html'
+        }), 
+        __metadata('design:paramtypes', [])
+    ], NavigationComponent);
     return NavigationComponent;
 }());
-NavigationComponent = __decorate([
-    core_1.Component({
-        moduleId: module.id,
-        selector: 'navigation',
-        templateUrl: './navigation.component.html'
-    }),
-    __metadata("design:paramtypes", [])
-], NavigationComponent);
 exports.NavigationComponent = NavigationComponent;
 //# sourceMappingURL=navigation.component.js.map
