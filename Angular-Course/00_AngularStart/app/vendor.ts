73781770ba932eea8f4299c42a65a1d674c422b2
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-03-23 06:31:15 +0100

    added webpack

--- Angular-Course/00_AngularStart/app/vendor.ts
+++ Angular-Course/00_AngularStart/app/vendor.ts
@@ -1,2 +1,7 @@
+import 'jquery/src/jquery';
 import 'bootstrap/dist/js/bootstrap';
+
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
+import '../favicon.ico';
