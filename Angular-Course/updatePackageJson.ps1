33e662a93e56be2a94e2f90f948686ded826d99a
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-08-24 16:29:35 +0200

    updated all packages to the latest version & added ps1 file to do this automatically

---
+++ Angular-Course/updatePackageJson.ps1
@@ -0,0 +1,16 @@
+﻿Get-ChildItem -recurse | 
+    where {$_.name -eq "package.json"} | 
+    foreach { 
+        cd $_.DirectoryName; 
+        Write-Output $_.DirectoryName; 
+    
+        if ( Test-Path "./node_modules" ) {
+            echo $_.DirectoryNamecls;
+            Remove-Item .\node_modules -Force -Recurse;
+        }
+
+        ncu -a
+
+        <# Remove-Item .\node_modules -Force -Recurse #>
+    }
+    cd ..
