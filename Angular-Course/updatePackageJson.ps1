0f30f93474bf77232a8c3422f0c702f1f39f3538
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-08-24 16:34:03 +0200

    ps1 is removing package-lock.json as well

--- Angular-Course/updatePackageJson.ps1
+++ Angular-Course/updatePackageJson.ps1
@@ -2,14 +2,20 @@
     where {$_.name -eq "package.json"} | 
     foreach { 
         cd $_.DirectoryName; 
-        Write-Output $_.DirectoryName; 
-    
+        Write-Output $_.DirectoryName;       
+        
         if ( Test-Path "./node_modules" ) {
             echo $_.DirectoryNamecls;
             Remove-Item .\node_modules -Force -Recurse;
         }
 
-        ncu -a
+        
+        if ( Test-Path "./package-lock.json" ) {
+            echo $_.DirectoryNamecls;
+             Remove-Item .\package-lock.json -Force;
+        }
+
+        <# ncu -a #>
 
         <# Remove-Item .\node_modules -Force -Recurse #>
     }
