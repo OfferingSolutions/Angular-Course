e99824cf63a8b79b7d52251791b1cb64a2196a0d
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-12-22 22:35:12 +0100

    added chapter forms

---
+++ chapters/09_Forms/end/src/app/models/foodItem.ts
@@ -0,0 +1,6 @@
+export class FoodItem {
+  id: number;
+  calories: number;
+  name: string;
+  created: Date;
+}
---
+++ chapters/09_Forms/end/src/app/validators/isNumber.validator.ts
@@ -0,0 +1,27 @@
+import { Directive } from '@angular/core';
+import {
+  Validator,
+  FormControl,
+  NG_VALIDATORS,
+  ValidationErrors
+} from '@angular/forms';
+
+@Directive({
+  selector: '[appIsNumber][ngModel]',
+  providers: [
+    { provide: NG_VALIDATORS, useExisting: IsNumberValidator, multi: true }
+  ]
+})
+export class IsNumberValidator implements Validator {
+  validate(c: FormControl): ValidationErrors | null {
+    if (isNaN(+c.value)) {
+      return {
+        isNumber: {
+          message: 'This could be a validation message'
+        }
+      };
+    }
+
+    return null;
+  }
+}
