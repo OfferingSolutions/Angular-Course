4b8b114109883ecbea20b22c65b464e4bc30daa4
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-12-22 22:09:23 +0100

    updated pipes

---
+++ chapters/08_Pipes/end/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {}
