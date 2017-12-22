71e45a57014743aa1bb0d16174a673e36715577a
FabianGosebrink <fabian.gosebrink@outlook.com>
2017-12-22 20:36:52 +0100

    added second part of components

---
+++ chapters/03_Components_Part_II/end/src/app/parent/parent.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parent',
+  templateUrl: './parent.component.html',
+  styleUrls: ['./parent.component.css']
+})
+export class ParentComponent implements OnInit {
+
+  public fruitList: string[] = ['Banana', 'Apples', 'Pineapples'];
+
+    constructor() {
+
+    }
+
+    ngOnInit() { }
+
+    public parentAddItem($event: any) {
+        this.fruitList.push($event.name);
+    }
+
+}
