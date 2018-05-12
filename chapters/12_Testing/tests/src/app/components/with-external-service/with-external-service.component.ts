2c10abd2a0b46ab6dc2859b88d84a3db0f24ca56
FabianGosebrink <fabian.gosebrink@outlook.com>
2018-05-12 21:50:03 +0200

    updated to angular 6

--- chapters/12_Testing/tests/src/app/components/with-external-service/with-external-service.component.ts
+++ chapters/12_Testing/tests/src/app/components/with-external-service/with-external-service.component.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { CustomHttpService } from '../../services/http-service/http.service';
 import { Component, OnInit } from '@angular/core';
 
