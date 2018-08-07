4dfa0605b698d0869da87f716b4d4a34783b47bf
FabianGosebrink <fabian.gosebrink@outlook.com>
2018-08-07 18:28:05 +0200

    updating WIP

--- chapters/12_Testing/todo-app/src/karma.conf.js
+++ chapters/12_Testing/todo-app/src/karma.conf.js
@@ -10,21 +10,17 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-mocha-reporter')
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly'],
+      dir: require('path').join(__dirname, '../coverage'),
+      reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    angularCli: {
-      environment: 'dev'
-    },
-    // reporters: ['mocha', 'progress', 'kjhtml'],
-    reporters: ['mocha', 'kjhtml'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
