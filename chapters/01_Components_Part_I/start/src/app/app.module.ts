a853e57c8a7a079c601b599730033e07bb44a9f9
FabianGosebrink <fabian.gosebrink@outlook.com>
2018-12-13 14:24:08 +0100

    corrected things

--- chapters/01_Components_Part_I/start/src/app/app.module.ts
+++ chapters/01_Components_Part_I/start/src/app/app.module.ts
@@ -4,17 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { ContentComponent } from './components/content/content.component';
-import { FormComponent } from './components/form/form.component';
-import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavigationComponent,
-    ContentComponent,
-    FormComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
