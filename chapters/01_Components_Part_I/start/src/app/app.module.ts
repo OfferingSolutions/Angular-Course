d8c1092ab0b3bac316dc6d07d4155dc091a0b0a5
FabianGosebrink <fabian.gosebrink@outlook.com>
2018-12-13 14:23:50 +0100

    updated to newest angular version

--- chapters/01_Components_Part_I/start/src/app/app.module.ts
+++ chapters/01_Components_Part_I/start/src/app/app.module.ts
@@ -3,11 +3,18 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
+import { ContentComponent } from './components/content/content.component';
+import { FormComponent } from './components/form/form.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    NavigationComponent,
+    ContentComponent,
+    FormComponent
+  ],
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
