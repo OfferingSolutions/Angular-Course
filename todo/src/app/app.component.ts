e52d248ca907dc4e55dcba66202738bdf1af3bdf
Tim Honermann <timhonermann@Tims-MacBook-Pro.local>
2024-06-30 12:24:51 +0200

    added todo component with form and databinding

--- todo/src/app/app.component.ts
+++ todo/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {TodoComponent} from "./todo/todo.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [
+    TodoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ todo/src/app/todo/todo.component.ts
@@ -0,0 +1,43 @@
+import {Component} from '@angular/core';
+import {Todo} from "../models/todo.models";
+import {FormControl, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule
+  ],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.scss'
+})
+export class TodoComponent {
+  readonly todoInput = new FormControl<string>('');
+
+  readonly todos: Todo[] = [];
+
+  addTodo(): void {
+    const todoValue = this.todoInput.value;
+
+    if (!todoValue) {
+      return;
+    }
+
+    const todo: Todo = {
+      id: '1',
+      value: todoValue,
+      done: false
+    };
+
+    this.todos.push(todo);
+  }
+}
