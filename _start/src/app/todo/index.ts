cc4c480fa9f44c964f77c46d528e11d97fd4e260
Tim Honermann <tim.honermann@windowslive.com>
2024-07-02 18:24:45 +0200

    remove old project

--- _start/src/app/todo/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './container/todo-main/todo-main.component';
--- _start/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: `https://sampletodobackend.azurewebsites.net/api/v1/`,
-  serverUrl: `https://sampletodobackend.azurewebsites.net/`,
-};
