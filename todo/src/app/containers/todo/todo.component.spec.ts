ae58194f3ea1ed05fcbeeb7476abd9171042e360
Tim Honermann <tim.honermann@windowslive.com>
2024-07-10 08:53:13 +0200

    use ng-mocks for provider

--- todo/src/app/containers/todo/todo.component.spec.ts
+++ todo/src/app/containers/todo/todo.component.spec.ts
@@ -3,38 +3,34 @@ import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {TodoComponent} from './todo.component';
 import {TodoService} from "../../services/todo.service";
 import {provideNoopAnimations} from "@angular/platform-browser/animations";
-import {CUSTOM_ELEMENTS_SCHEMA, signal} from "@angular/core";
+import {TodoListComponent} from "../../presentationals/todo-list/todo-list.component";
+import {TodoHeaderComponent} from "../../presentationals/todo-header/todo-header.component";
+import {TodoFormComponent} from "../../presentationals/todo-form/todo-form.component";
+import {MockComponent, MockProvider} from "ng-mocks";
+import {signal} from "@angular/core";
 
 describe('TodoComponent', () => {
   let component: TodoComponent;
   let fixture: ComponentFixture<TodoComponent>;
-  let todoService: TodoService;
-
-  const todoServiceSpy = jasmine.createSpyObj('TodoService', {
-    'count': () => signal(0),
-    'openCount': () => signal(0),
-    'doneCount': () => signal(0),
-    'sortedTodos': () => void 0,
-    'getAll': () => void 0,
-    'add': () => void 0,
-    'update': () => void 0,
-    'delete': () => void 0
-  });
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TodoComponent],
       providers: [
         provideNoopAnimations(),
-        { provide: TodoService, useValue: todoServiceSpy }
+        MockProvider(TodoService, {
+          count: signal(0),
+          doneCount: signal(0),
+          openCount: signal(0),
+          sortedTodos: signal([])
+        })
       ]
     })
       .overrideComponent(TodoComponent, {
-        set: { imports: [], schemas: [CUSTOM_ELEMENTS_SCHEMA] }
+        remove: { imports: [TodoListComponent, TodoHeaderComponent, TodoFormComponent] },
+        add: { imports: [MockComponent(TodoListComponent), MockComponent(TodoHeaderComponent), MockComponent(TodoFormComponent)] },
       })
     .compileComponents();
-
-    todoService = TestBed.inject(TodoService);
 
     fixture = TestBed.createComponent(TodoComponent);
     component = fixture.componentInstance;
