311b9642594c19b2562bdccf1885ff9235d63470
Tim Honermann <tim.honermann@windowslive.com>
2024-07-10 09:00:50 +0200

    Test Capitalize Pipe

--- todo/src/app/pipes/capitalize.pipe.spec.ts
+++ todo/src/app/pipes/capitalize.pipe.spec.ts
@@ -1,8 +1,22 @@
-import { CapitalizePipe } from './capitalize.pipe';
+import {CapitalizePipe} from './capitalize.pipe';
 
 describe('CapitalizePipe', () => {
   it('create an instance', () => {
     const pipe = new CapitalizePipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should return value with first character capitalized', () => {
+    // arrange
+    const value = 'my lowercase value';
+    const expectedValue = 'My lowercase value';
+
+    const pipe = new CapitalizePipe();
+
+    // act
+    const result = pipe.transform(value);
+
+    // assert
+    expect(result).toEqual(expectedValue)
+  })
 });
