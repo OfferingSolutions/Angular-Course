e33b10932f332e0c462340f5da03f17bf39ece08
FabianGosebrink <fabian.gosebrink@outlook.com>
2024-09-23 20:53:50 +0200

    Added list component

--- todo/src/app/containers/todo/todo.component.ts
+++ todo/src/app/containers/todo/todo.component.ts
@@ -7,6 +7,7 @@ import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {TodoHeaderComponent} from "../../presentationals/todo-header/todo-header.component";
 import {TodoFormComponent} from "../../presentationals/todo-form/todo-form.component";
+import { TodoListComponent } from "../../presentationals/todo-list/todo-list.component";
 
 @Component({
   selector: 'app-todo',
@@ -19,10 +20,11 @@ import {TodoFormComponent} from "../../presentationals/todo-form/todo-form.compo
     MatIconModule,
     ReactiveFormsModule,
     TodoHeaderComponent,
-    TodoFormComponent
+    TodoFormComponent,
+    TodoListComponent,
   ],
   templateUrl: './todo.component.html',
-  styleUrl: './todo.component.scss'
+  styleUrl: './todo.component.scss',
 })
 export class TodoComponent {
   readonly todos = signal<Todo[]>([]);
@@ -33,7 +35,7 @@ export class TodoComponent {
     const todo: Todo = {
       id: '1',
       value,
-      done: false
+      done: false,
     };
 
     this.todos.update((todos) => [...todos, todo]);
