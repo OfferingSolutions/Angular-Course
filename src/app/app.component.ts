573d5d7cd1a845964690fb8a8ab27ef3dc7e6021
Tim Honermann <tim.honermann@windowslive.com>
2025-01-12 09:56:11 +0100

    angular material 19 upgrade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import {Component} from '@angular/core';
-import {TodoComponent} from "./containers/todo/todo.component";
 import {RouterOutlet} from "@angular/router";
 
 @Component({
     selector: 'app-root',
     imports: [
-        TodoComponent,
         RouterOutlet
     ],
     templateUrl: './app.component.html',
