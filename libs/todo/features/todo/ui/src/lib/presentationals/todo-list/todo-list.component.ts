dfdf1e563ea94d79617d3b7a9c4745f3493a0fce
Tim Honermann <tim.honermann@windowslive.com>
2025-01-12 11:23:22 +0100

    ngrx signal store 19 upgrade

--- libs/todo/features/todo/ui/src/lib/presentationals/todo-list/todo-list.component.ts
+++ libs/todo/features/todo/ui/src/lib/presentationals/todo-list/todo-list.component.ts
@@ -1,15 +1,15 @@
-import {Component, input, output} from '@angular/core';
-import {MatButton} from '@angular/material/button';
-import {MatCheckbox} from '@angular/material/checkbox';
-import {CapitalizePipe} from '../../pipes/capitalize.pipe';
-import {RouterLink} from '@angular/router';
-import {Todo} from "@todo/todo-domain";
+import { Component, input, output } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { CapitalizePipe } from '../../pipes/capitalize.pipe';
+import { Todo } from '@todo/todo-domain';
+import { RouterLink } from '@angular/router';
 
 @Component({
-    selector: 'app-todo-list',
-    imports: [MatButton, MatCheckbox, CapitalizePipe, RouterLink],
-    templateUrl: './todo-list.component.html',
-    styleUrl: './todo-list.component.scss'
+  selector: 'app-todo-list',
+  imports: [MatButton, MatCheckbox, CapitalizePipe, RouterLink],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.scss',
 })
 export class TodoListComponent {
   todos = input.required<Todo[]>();
@@ -19,8 +19,12 @@ export class TodoListComponent {
   deleteClicked = output<string>();
 
   toggleDone(todo: Todo) {
-    todo.done = !todo.done;
-    this.doneToggled.emit(todo);
+    const updatedTodo: Todo = {
+      ...todo,
+      done: !todo.done,
+    };
+
+    this.doneToggled.emit(updatedTodo);
   }
 
   delete(id: string) {
