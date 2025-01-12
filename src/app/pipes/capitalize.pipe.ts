9d2b8f07355a20317a45df739dca7ef1376aaf21
Tim Honermann <tim.honermann@windowslive.com>
2025-01-12 09:54:58 +0100

    angular 19 upgrade

--- src/app/pipes/capitalize.pipe.ts
+++ src/app/pipes/capitalize.pipe.ts
@@ -1,8 +1,7 @@
 import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
-  name: 'capitalize',
-  standalone: true
+  name: 'capitalize'
 })
 export class CapitalizePipe implements PipeTransform {
 
